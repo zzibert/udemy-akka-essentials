3fda268c887590e279e07e914e6c78be7240aa26
Žan Žibert <zzibert@plume.com>
2020-09-24 13:44:51 +0200

    Changing Actor Behavior

---
+++ src/main/scala/Part2Actors/ChangingActorBehavior.scala
@@ -0,0 +1,79 @@
+package Part2Actors
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+
+object ChangingActorBehavior extends App {
+
+  object FussyKid {
+    case object KidAccept
+    case object KidReject
+    val HAPPY = "happy"
+    val SAD = "sad"
+  }
+
+  class FussyKid extends Actor {
+    import FussyKid._
+    import Mom._
+    var state = HAPPY
+    override def receive: Receive = {
+      case Food(VEGETABLE) => state = SAD
+      case Food(CHOCOLATE) => state = HAPPY
+      case Ask(_) =>
+        if (state == HAPPY) sender ! KidAccept
+        else sender ! KidReject
+    }
+  }
+
+  class StateLessFussyKid extends Actor {
+    import FussyKid._
+    import Mom._
+
+    override def receive: Receive = happyReceive
+
+    def happyReceive: Receive = {
+      case Food(VEGETABLE) => context.become(sadReceive)
+      case Food(CHOCOLATE) =>
+      case Ask(_) => sender ! KidAccept
+    }
+
+    def sadReceive: Receive = {
+      case Food(VEGETABLE) =>
+      case Food(CHOCOLATE) => context.become(happyReceive)
+      case Ask(_) => sender ! KidReject
+    }
+  }
+
+  object Mom {
+    case class MomStart(kid: ActorRef)
+    case class GiveChocolate(kid: ActorRef)
+    case class Food(food: String)
+    case class Ask(message: String) // do you want to play
+    val VEGETABLE = "veggies"
+    val CHOCOLATE = "chocolate"
+  }
+
+  class Mom extends Actor {
+    import Mom._
+    import FussyKid._
+    override def receive: Receive = {
+      case KidAccept => println("yey my kid is happy")
+      case KidReject => println("my kid is sad, but he is healthy")
+      case MomStart(kid) =>
+        kid ! Food(VEGETABLE)
+        kid ! Ask("Do you want to play")
+      case GiveChocolate(kid) =>
+        kid ! Food(CHOCOLATE)
+        kid ! Ask("do you want to play")
+
+    }
+  }
+
+  val system = ActorSystem("ChangingActorBehaviorDemo")
+  val fussyKid = system.actorOf(Props[FussyKid], "fussyKid")
+  val mom = system.actorOf(Props[Mom], "mom")
+  val stateLessFussyKid = system.actorOf(Props[StateLessFussyKid], "statelessfussykid")
+  import Mom._
+  mom ! MomStart(stateLessFussyKid)
+  mom ! GiveChocolate(stateLessFussyKid)
+
+}
