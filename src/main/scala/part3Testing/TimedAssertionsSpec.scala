015a2da5c15b7ec651a7a8143395ce80667ce763
Zan Zibert <zan_zibert@hotmail.com>
2020-09-27 16:23:28 +0200

    Timed Assertions

---
+++ src/main/scala/part3Testing/TimedAssertionsSpec.scala
@@ -0,0 +1,74 @@
+package part3Testing
+
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import com.typesafe.config.ConfigFactory
+import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+
+import scala.concurrent.duration.DurationInt
+import scala.util.Random
+
+class TimedAssertionsSpec extends TestKit(ActorSystem("TimeAssertionsSpec", ConfigFactory.load().getConfig("specialTimedAssertionsConfig")))
+  with ImplicitSender
+  with WordSpecLike
+  with BeforeAndAfterAll  {
+
+  override def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  import TimedAssertionsSpec._
+
+  "A worker actor" should {
+    val workerActor = system.actorOf(Props[WorkerActor])
+
+    "reply with the meaning of life in a timely manner" in {
+      within(500 millis, 1 second) {
+        workerActor ! "work"
+        expectMsg(WorkResult(42))
+      }
+    }
+
+    "reply with valid work at a reasonable cadence" in {
+      within(1 second) {
+        workerActor ! "workSequence"
+
+        val results: Seq[Int] = receiveWhile[Int](max = 2 seconds, idle=50 millis, messages = 10) {
+          case WorkResult(result) => result
+        }
+
+        assert(results.sum > 9)
+      }
+    }
+
+    " reply to a test probe in a timely manner" in {
+      within(1 second) {
+        val probe = TestProbe()
+        probe.send(workerActor, "work")
+        probe.expectMsg(WorkResult(42)) // timeout of 0.3 seconds
+      }
+    }
+  }
+
+}
+
+object TimedAssertionsSpec {
+
+  case class WorkResult(result: Int)
+
+  // testing scenario
+  class WorkerActor extends Actor {
+    override def receive: Receive = {
+      case "work" => // simulate long computation
+        Thread.sleep(500)
+        sender ! WorkResult(42)
+
+      case "workSequence" =>
+        val r = new Random()
+        for (_ <- 1 to 10) {
+          Thread.sleep(r.nextInt(50))
+          sender ! WorkResult(1)
+        }
+    }
+  }
+}
